47199d609730f11283738d0759ac1e7c0c446379
abbam1 <abba8521@gmail.com>
2024-05-17 15:50:17 +0000

    route proteger

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,42 @@
+import { useboursesStore } from "@/store/bourses";
 import { createRouter, createWebHistory } from "vue-router";
-import DashboardVue from "@/views/Dashboard.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: "/",
-    //   name: "home",
-    //   component: DashboardVue,
-    // },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
+    {
+      path: "/",
+      name: "login",
+      component: () => import("../auth/Login.vue"),
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/Dashboard.vue"),
+      meta: {
+        requiresAuth: true,
+      },
+    },
   ],
+});
+
+router.beforeEach(async (to, from) => {
+
+const localStorage_token = localStorage.getItem('loginInfo').token
+
+
+  if (
+    // if the user is not authenticated
+    !localStorage_token &&
+    // if the route name is not login 
+    to.name !== "login"
+  ) {
+    //  then redirect the user to the login page
+    return { name: "login" };
+  }
 });
 
 export default router;
